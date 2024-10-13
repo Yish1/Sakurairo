511c22a5afefe4861253d033b268123e3180a01b
Yish1 <934936015@qq.com>
2024-10-13 23:20:53 +0800

    个性化

--- 404.php
+++ 404.php
@@ -5,9 +5,9 @@ get_header();
 <head>
 </head>
 <img src="https://cmxz.top/images/404/very_sorry.webp"><br></br>
-<div class="err-button back">
-            <a id="golast" href=javascript:history.go(-1);><?php _e('return to previous page', 'sakurairo');/*返回上一页*/ ?></a>
-            <a id="gohome" href="<?php bloginfo('url'); ?>"><?php _e('return to home page', 'sakurairo');/*返回主页*/ ?></a>
+<div class="err-button back" style="display: flex; flex-direction: row; flex-wrap: wrap; align-content: center; justify-content: center;">
+            <a id="golast" href="javascript:history.go(-1);"><?php _e('Return to previous page', 'sakurairo'); ?></a>
+            <a id="gohome" href="<?php echo esc_url(home_url('/')); ?>"><?php _e('Return to home page', 'sakurairo'); ?></a>
         </div>
 <br></br><br>
 <figure class="wp-block-image size-large"><img decoding="async" class="lazyload" data-src="https://cmxz.top/images/404/api.php" class="lazyload" data-src="https://cmxz.top/images/404/api.php" src="https://cdn.cmxz.top/images/load.svg" onerror="imgError(this)" onerror="imgError(this)" alt=""></figure>
