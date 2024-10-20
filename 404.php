bbf4dba384a5b4b41c3a16f161d4e480e2460e54
Yish1 <934936015@qq.com>
2024-10-20 16:46:07 +0800

    custom

--- 404.php
+++ 404.php
@@ -5,12 +5,17 @@ get_header();
 <head>
 </head>
 <img src="https://cmxz.top/images/404/very_sorry.webp"><br></br>
+
 <div class="err-button back" style="display: flex; flex-direction: row; flex-wrap: wrap; align-content: center; justify-content: center;">
-            <a id="golast" href="javascript:history.go(-1);"><?php _e('Return to previous page', 'sakurairo'); ?></a>
-            <a id="gohome" href="<?php echo esc_url(home_url('/')); ?>"><?php _e('Return to home page', 'sakurairo'); ?></a>
-        </div>
+    <a id="golast" href="javascript:history.go(-1);"><?php _e('Return to previous page', 'sakurairo'); ?></a>
+    <a id="gohome" href="<?php echo esc_url(home_url('/')); ?>"><?php _e('Return to home page', 'sakurairo'); ?></a>
+</div>
 <br></br><br>
-<figure class="wp-block-image size-large"><img decoding="async" class="lazyload" data-src="https://cmxz.top/images/404/api.php" class="lazyload" data-src="https://cmxz.top/images/404/api.php" src="https://cdn.cmxz.top/images/load.svg" onerror="imgError(this)" onerror="imgError(this)" alt=""></figure>
-<noscript><img decoding="async" class="lazyload" data-src="https://cmxz.top/images/404/api.php" src="https://cmxz.top/images/404/api.php" onerror="imgError(this)" alt=""></figure></noscript>
+
+<figure class="wp-block-image size-large">
+    <img decoding="async" src="https://cmxz.top/images/404/api.php" onerror="imgError(this)" alt="">
+</figure>
+
 <?php
 get_footer();
+?>
